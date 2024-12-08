5a9f17235f149611545ad0de91ef7d72e76ce35f
oSLaYN <rodrigo.6640@gmail.com>
2024-12-08 13:04:59 +0000

    Comments for better reading & understanding

--- books.js
+++ books.js
@@ -5,8 +5,10 @@ const path = require('path');
 const supabase = require('./config/supabase');
 const { type } = require('os');
 
+// Função responsável por devolver todos os livros da base de dados
 functions.mostrarLivros = async function() {
     try {
+        // Procurar na base de dados toda a informação de todos os livros e devolver por ordem ascendente de ID
         const {data, error} = await supabase.from('books').select().order('ID', {ascending: true});
         return JSON.parse(JSON.stringify(data));
     } catch (error) {
@@ -15,8 +17,10 @@ functions.mostrarLivros = async function() {
     }
 };
 
+// Função responsável por devolver o livro da base de dados
 functions.showBookByID = async function(id) {
     try {
+        // Procurar na base de dados toda a informação sobre o livro com tal ID e devolver
         const {data, error} = await supabase.from('books').select().eq('ID', id);
         if (data.length === 0) { return {message: 'Livro Inexistente'}; }
         return data[0];
@@ -26,20 +30,25 @@ functions.showBookByID = async function(id) {
     }
 };
 
+// Função responsável por atualizar o livro na base de dados
 functions.updateBookByID = async function(id, newdata) {
     try {
+        // Procurar o livro na base de dados pelo seu ID e se existe ou não.
         var {data, error} = await supabase.from('books').select().eq('ID', id);
         if (data.length === 0) { return {message: 'Livro Inexistente'}; }
+        // Criar a nova informação a ser atualizada no campo de dados com campos novos ou o já existente com condições "if-else"
         const dataToUpdate = {date: (newdata.date ? newdata.date : data[0].YEAR),
             title: (newdata.title ? newdata.title : data[0].TITLE),
             language: (newdata.language ? newdata.language : data[0].LANGUAGE),
             authors: (newdata.authors ? newdata.authors : data[0].AUTHORS)}
         try {
+            // Atualizar os campos do livro por novos dados com a informação anteriormente definida
             const {error} = await supabase.from('books').update({YEAR: dataToUpdate.date,
                                                                                 TITLE: dataToUpdate.title,
                                                                                 LANGUAGE: dataToUpdate.language,
                                                                                 AUTHORS: dataToUpdate.authors}).eq('ID', id);
             try {
+                // Pesquisar o livro novamente na base de dados, na qual já está atualizado e será devolvido.
                 const {data, error} = await supabase.from('books').select().eq('ID', id);
                 return data[0];
             } catch(error) {
@@ -55,61 +64,5 @@ functions.updateBookByID = async function(id, newdata) {
         return {message: 'Erro ao Mostrar os Livros'};
     }
 }
-
-/* exports.adicionarLivro = async (req, res) => {
-    const { ID, YEAR, TITLE, LANGUAGE, AUTHORS } = req.body;
-    if (ID === undefined || typeof ID !== 'number' || isNaN(ID)) {
-        return res.status(400).send('Erro: ID inválido ou não numérico.');
-    }
-    const newBook = { ID, YEAR, TITLE, LANGUAGE, AUTHORS };
-
-    try {
-        const docRef = db.collection('catalogs').doc(String(ID));
-        const docSnapshot = await docRef.get();
-        if (docSnapshot.exists) {
-            return res.status(400).send('Erro: Livro com este ID já existe.');
-        }
-        await docRef.set(newBook);
-        res.status(201).send('Livro Adicionado com Sucesso');
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Erro ao Adicionar Livro');
-    }
-};
-
-exports.atualizarLivro = async (req, res) => {
-    const { ID, YEAR, TITLE, LANGUAGE, AUTHORS } = req.body;
-    if (ID === undefined || typeof ID !== 'number' || isNaN(ID)) {
-        return res.status(400).send('Erro: ID inválido ou não numérico.');
-    }
-
-    try {
-        const docRef = db.collection('catalogs').doc(String(ID));
-        const docSnapshot = await docRef.get();
-        if (!docSnapshot.exists) {
-            return res.status(404).send('Erro: Livro não encontrado.');
-        }
-        await docRef.update({ YEAR, TITLE, LANGUAGE, AUTHORS });
-        res.status(200).send('Livro Atualizado com Sucesso');
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Erro ao Atualizar o Livro');
-    }
-};
-
-exports.apagarLivro = async (req, res) => {
-    const { ID } = req.body;
-    if (ID === undefined || typeof ID !== 'number' || isNaN(ID)) {
-        return res.status(400).send('Erro: ID inválido ou não numérico.');
-    }
-
-    try {
-        await db.collection('catalogs').doc(String(ID)).delete();
-        res.status(200).send('Livro Apagado com Sucesso');
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Erro ao Apagar o Livro');
-    }
-}; */
 
 module.exports = functions;
